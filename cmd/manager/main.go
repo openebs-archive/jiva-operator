0a3b6b29dee56f1cdcfbc6c130bca256ec352e05
Utkarsh <utkarshm@jfrog.com>
2020-10-11 11:05:24 +0530

    fix(modules): change broken modules
    Signed-off-by: Utkarsh <utkarshm@jfrog.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -23,8 +23,6 @@ import (
 	"os"
 	"runtime"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 
 	"github.com/openebs/jiva-operator/pkg/apis"
@@ -36,7 +34,6 @@ import (
 	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
-	"github.com/operator-framework/operator-sdk/pkg/restmapper"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	"github.com/spf13/pflag"
 	v1 "k8s.io/api/core/v1"
@@ -109,7 +106,6 @@ func main() {
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{
 		Namespace:          namespace,
-		MapperProvider:     restmapper.NewDynamicRESTMapper,
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 		SyncPeriod:         &mgr.SyncPeriod,
 		RetryPeriod:        &mgr.RetryPeriod,
