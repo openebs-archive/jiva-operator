946f9f56632d87b99322a05c7433f867c75d986e
Sonia Singla <soniasingla.1812@gmail.com>
2021-05-04 13:06:10 +0000

    Unused openebs function removal
    Signed-off-by: Sonia Singla <soniasingla.1812@gmail.com>

--- tests/suite_test.go
+++ tests/suite_test.go
@@ -32,10 +32,6 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
-var (
-	openebsNamespace = "openebs"
-)
-
 func TestMtest(t *testing.T) {
 	//  if os.Getenv("E2ETEST") == "" {
 	//  t.Skip("Run under e2e/")
